77d73c35bb4b96f8f788c1baa54442970c23c83f
macsakarn <macsakarn@gmail.com>
2022-02-08 20:55:10 +0700

    add Components & init Sockect Client

--- FakeClient/fake-client-next/pages/index.js
+++ FakeClient/fake-client-next/pages/index.js
@@ -1,9 +1,37 @@
+import { io } from "socket.io-client";
+import Card from "../components/Chat";
+import Messages from "../components/Messages";
+import { Container, Box } from "@mui/material";
+import React, { useEffect, useState } from 'react'
 
+const Name = ["Mac","TK","Fluke","Tuuuu","Bank"]
 
 export default function Home() {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    // const newSocket = io(`http://________:______`);
+    // setSocket(newSocket);
+    // return () => newSocket.close();
+  }, [setSocket]);
+  
   return (
-    <div>
-      
-    </div>
-  )
+    <Container maxWidth="xl">
+      <Box
+        sx={{
+          display: "flex",
+          mb:4,
+          justifyContent: "space-between",
+          gap: 2,
+        }}
+      >
+        {
+          Name.map((val,index)=>{
+           return <Card name={val} key={index} socket={socket}  />
+          })
+        }
+      </Box>
+        <Messages socket={socket}/>
+    </Container>
+  );
 }
