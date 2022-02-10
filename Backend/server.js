73ec5723e86e44c6e445134f3bdaa22a5eddc8d2
takai <nipas2013@gmail.com>
2022-02-10 12:03:18 +0700

    update: change socket connection code in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,22 @@
 import express from "express";
 import socketOption from "./constants/socketServerOptions.js";
-import Websocket from "./utils/Websocket.js";
 import { Server } from "socket.io";
 import { createServer } from "http";
+import buildChatSocketClass from "./utils/WebSocketClass.js";
 
 // Server side socket.io connection code
 const server = express();
 const httpServer = createServer(server);
 
-// Declare socket to global 
-global.io = new Server(httpServer, socketOption);
-global.io.on('connection', Websocket.connection);
+// Build new Socket Class
+const io = new Server(httpServer, socketOption);
+buildChatSocketClass(io);
 
 // server listening for socket 
 httpServer.listen(8080, () =>
   console.log(`server run on port ${8080}`)
 );
 
+// Declare socket to global 
+// global.io = new Server(httpServer, socketOption);
+// global.io.of('/world_chat').on('connection', Websocket.connection);
