748c3521c720f8e671b6d2a142ca26e89046ba1c
bankjirayuth <53044682+bankjirayuth@users.noreply.github.com>
2022-02-13 18:14:33 +0700

    Update: Use Redux in frontend code , Add: Modal username component

---
+++ Frontend/wannaplay-webapp/pages/reducers/inputReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  username: "",
+};
+
+const inputReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INPUT_USERNAME":
+        return {username: action.data};
+    default:
+      return state;
+  }
+};
+
+export default inputReducer;
