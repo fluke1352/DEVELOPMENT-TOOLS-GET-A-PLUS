0ea418173fed4d04fabcb9d6994ca1004a275917
takai <nipas2013@gmail.com>
2022-04-16 18:39:47 +0700

    update : fecth game info logic

--- Backend/controller/game.controller.js
+++ Backend/controller/game.controller.js
@@ -1,9 +1,12 @@
-const fetchGameInfo = (req, res) => {
-    try {
+import GameModel from "../models/game.model"
 
+const fetchGameInfo = async (req, res) => {
+    try {
+        const allGame = GameModel.find()
+        res.status(200).json(allGame)
     }
     catch (err) {
-
+        res.status(500).json({ error : err })
     }
 }
 
