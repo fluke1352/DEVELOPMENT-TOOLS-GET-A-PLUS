d61c9c9f5e1399c38a8402dc96eb4d4824a3a5bb
takai <nipas2013@gmail.com>
2022-04-16 21:43:13 +0700

    update mongo uri

--- Backend/constants/mongoConfigs.js
+++ Backend/constants/mongoConfigs.js
@@ -1,5 +1,5 @@
 const configs = {
-    mongouri : process.env.MONGOURI || ''
+    mongouri : "mongodb+srv://it62070035:xitgmLwmp12q@cluster0.znxaw.mongodb.net/Games?retryWrites=true&w=majority"
 }
 
 export default configs;
